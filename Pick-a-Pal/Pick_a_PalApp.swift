363b935e6abe6e2e23ab1acb85b3140abf40a8af
Jordan Haynes <88909614+JordanCautious@users.noreply.github.com>
2024-07-12 11:13:32 -0400

    Initial Commit

---
+++ Pick-a-Pal/Pick_a_PalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pick_a_PalApp.swift
+//  Pick-a-Pal
+//
+//  Created by Jordan Haynes on 7/12/24.
+//
+
+import SwiftUI
+
+@main
+struct Pick_a_PalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
